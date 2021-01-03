93c0504058f70bd12dec52ff0972de963a26d2bd
Nathan Griffiths <ngriffiths@postmedia.com>
2021-01-02 18:13:32 -0800

    fixed a bug in the url param code. droped turf.js

--- src/js/helper-functions.js
+++ src/js/helper-functions.js
@@ -1,20 +1,20 @@
 const helper = {
 	getPrCode() {
-		let province;
 		const defaultPrCode = 'BC';
 		const queryString = window.location.search;
 		const urlParams = new URLSearchParams(queryString);
 		const prCode = urlParams.get('prov');
 
-		return this.validProvinceCodes.includes(prCode.toUpperCase()) ? prCode.toUpperCase() : defaultPrCode;
+		// is there a code?
+		const code = prCode ? prCode.toUpperCase() : defaultPrCode;
+		return this.validProvinceCodes.includes(code) ? code : defaultPrCode;
 	},
-
 	map(num, in_min, in_max, out_min, out_max) {
 		return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	},
 	months: ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'],
 	numberWithCommas(x) {
-	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	},
 	validProvinceCodes: ['YT','NT','NU','BC','AB','SK','MB','ON','QC','NL','NB','PE','NS']
 };
