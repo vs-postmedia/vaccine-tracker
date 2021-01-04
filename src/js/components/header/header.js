9c7d98c8b3c9f99684ccf208cae19945d5014079
Nathan Griffiths <ngriffiths@postmedia.com>
2021-01-03 21:11:35 -0800

    small style edits

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -4,10 +4,9 @@ import './header.css';
 // import template from 'header-template';
 const init = async (data, provCode) => {
 	const prov = data.filter(d => d.code === provCode)[0];
-	const provName = prov.name;
 
 	return `
-		<h1>${provName} has administered <span class="highlight">${helper.numberWithCommas(parseInt(prov['Doses administered']))} doses</span> of COVID-19 vaccines so far – roughly <span class="highlight">${prov['% vaccinated']}%</span> of the province.</h1>
+		<h1>${prov.name} has administered <span class="highlight">${helper.numberWithCommas(parseInt(prov['Doses administered']))} doses</span> of COVID-19 vaccines so far – roughly <span class="highlight">${prov['% vaccinated']}%</span> of the province.</h1>
 	`;
 }
 
