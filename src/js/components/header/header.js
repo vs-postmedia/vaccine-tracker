307dcdc8cd19864024a186b2c8e13de4961a201b
Nathan Griffiths <ngriffiths@postmedia.com>
2021-02-10 21:59:45 -0800

    header style updates

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -8,10 +8,10 @@ const init = async (data, provCode) => {
 
 	if (provCode !== 'CA') {
 		header = `
-			<h1>At least <span class="highlight">${helper.numberWithCommas(parseInt(prov.full_vax))}</span> people in ${prov.name} have been fully vaccinated against the coronavirus. Another <span class="highlight">${helper.numberWithCommas(parseInt(prov.doses_admin - (prov.full_vax * 2)))}</span> have received one vaccine dose.</h1>
+			<h2>At least <span class="highlight">${helper.numberWithCommas(parseInt(prov.full_vax))}</span> people in ${prov.name} have been fully vaccinated against the coronavirus. Another <span class="highlight">${helper.numberWithCommas(parseInt(prov.doses_admin - (prov.full_vax * 2)))}</span> have received one vaccine dose.</h2>
 		`;
 	} else {
-		header = '<h1>Doses of COVID-19 vaccine administered per 100,000 people</h1>'
+		header = '<h2>Doses of COVID-19 vaccine administered per 100,000 people</h2>'
 	}
 
 	return header;
